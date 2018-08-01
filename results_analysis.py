bcb204cef6ec4d2cdc275bc6dbf05cbcf5b818a0
mg515 <miha.garafolj@gmail.com>
2018-08-01 11:56:12 +0200

    added file for results analysis

---
+++ results_analysis.py
@@ -0,0 +1,11 @@
+
+
+
+from utilities import read_results
+
+path = '/media/ostalo/MihaGarafolj/ME_data/CASME2_Optical_Aug/Classification/Result/predicts_casme2_optical_aug_c3dwithPadding.txt'
+table,acc,cm = read_results(path)
+
+
+
+
