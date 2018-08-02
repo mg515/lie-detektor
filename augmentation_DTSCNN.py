7f6cd02e4e0b1d011266967fe919d7e31964e87e
mg515 <miha.garafolj@gmail.com>
2018-08-02 10:25:32 +0200

    processing the 2nd OF dataset

--- augmentation_DTSCNN.py
+++ augmentation_DTSCNN.py
@@ -8,13 +8,14 @@ from list_databases import load_db
 # data augmentation and sampling as done in the DTSCNN paper
 ###
 
-root_db_path = "/home/miha/Documents/ME_data/"
+#root_db_path = "/home/miha/Documents/ME_data/"
+root_db_path = "/media/ostalo/MihaGarafolj/ME_data"
 ######################################################
 
-list_dB = ['CASME2_Optical']
+list_dB = ['CASME2_Optical2']
 objective_flag = 'st'
 spatial_size = 224
 r, w, subjects, samples, n_exp, VidPerSubject, vidList, timesteps_TIM, data_dim, channel, table, listOfIgnoredSamples, db_home, db_images, cross_db_flag = load_db(root_db_path, list_dB, spatial_size, objective_flag)
 resizedFlag = 0
 
-augmentation_casme(db_images, db_images+"augmentation/", 200, table, resizedFlag, r, w)
+augmentation_casme(db_images, db_images+"augmentation/", 400, table, resizedFlag, r, w)
