f9fc299266b64525dffd92e1e692cc995fbf7ef0
mihag <miha.garafolj@gmail.com>
2018-08-15 14:57:00 +0200

    updated

--- augmentation_DTSCNN.py
+++ augmentation_DTSCNN.py
@@ -12,7 +12,7 @@ from list_databases import load_db
 root_db_path = "/media/ostalo/MihaGarafolj/ME_data/"
 ######################################################
 
-list_dB = ['CASME2_flowNet_Aug']
+list_dB = ['CASME2_Optical']
 objective_flag = 'st'
 spatial_size = 224
 r, w, subjects, samples, n_exp, VidPerSubject, vidList, timesteps_TIM, data_dim, channel, table, listOfIgnoredSamples, db_home, db_images, cross_db_flag = load_db(root_db_path, list_dB, spatial_size, objective_flag)
