457bd07ef2b59173f7895376b116e9fc0fcfa852
mg515 <miha.garafolj@gmail.com>
2018-08-15 12:56:19 +0200

    updated

--- results_analysis.py
+++ results_analysis.py
@@ -18,6 +18,10 @@ from utilities import read_results
 path = '/media/ostalo/MihaGarafolj/ME_data/CASME2_Optical_TIM20_Aug/Classification/Result/predicts_casme2_tim20_optical_aug.txt'
 table,acc,cm = read_results(path)
 
+
+path = '/media/ostalo/MihaGarafolj/ME_data/CASME2_Optical2_Aug/Classification/Result/predicts_casme2_optical2_aug.txt'
+table,acc,cm = read_results(path)
+
 print(table)
 print(acc)
 print(cm)
