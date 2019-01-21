a45c32a803c04719cfbc048800145ebd5d316a54
mihag <miha.garafolj@gmail.com>
2019-01-21 19:08:10 +0100

    named the dense layers in vgg

--- models.py
+++ models.py
@@ -129,9 +129,9 @@ def VGG_16(spatial_size, classes, channels, channel_first=False, weights_path=No
 	model.add(MaxPooling2D((2,2), strides=(2,2))) # 33
 
 	model.add(Flatten())
-	model.add(Dense(4096, activation='relu')) # 34
+	model.add(Dense(4096, activation='relu', name = 'dense_1')) # 34
 	model.add(Dropout(0.5))
-	model.add(Dense(4096, activation='relu')) # 35
+	model.add(Dense(4096, activation='relu', name = 'dense_2')) # 35
 	model.add(Dropout(0.5))
 	model.add(Dense(2622, activation='softmax')) # Dropped
 
